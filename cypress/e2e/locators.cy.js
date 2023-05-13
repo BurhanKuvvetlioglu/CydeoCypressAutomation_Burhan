5455e5e7486337a77ac864f864cadbebb379c733
Burhan Kuvvetlioglu <bkuvvetlioglu@hotmail.com>
2023-05-13 17:18:32 +0300

    add: inputs added

--- cypress/e2e/locators.cy.js
+++ cypress/e2e/locators.cy.js
@@ -1,56 +1,54 @@
 /// <reference types="cypress" />
 
 describe('Find or get Elemnts by using Different locators', () => {
-    
-    beforeEach(() => {
-        // runs before each test case, resembles BeforeMethod in TestNG
-        cy.clearCookies();
-        cy.visit('login');
-    })
-    
-    it('Check different locators strategies', () => {
-    
-        // By CSS locator
-        cy.get("input[name='username']").type("CydeoStudent") // every statement creates an object to be intearcted, and next command makes operation to the object created at the previous statement
-        // attribute name and value 
-        cy.get("[type='text']").clear();  // clears what is typed
-        // tagName
-        cy.get("input").each((item, index, list) => {
-            // assert the length of the list is 2
-            expect(list).to.have.length(2);
-            expect(item).to.have.attr("type");
-        })
+  beforeEach(() => {
+    // runs before each test case, resembles BeforeMethod in TestNG
+    cy.clearCookies();
+    cy.visit('login');
+  });
 
-        // by attribute name
-        cy.get("[type]");
+  it('Check different locators strategies', () => {
+    // By CSS locator
+    cy.get("input[name='username']").type('CydeoStudent'); // every statement creates an object to be intearcted, and next command makes operation to the object created at the previous statement
+    // attribute name and value
+    cy.get("[type='text']").clear(); // clears what is typed
+    // tagName
+    cy.get('input').each((item, index, list) => {
+      // assert the length of the list is 2
+      expect(list).to.have.length(2);
+      expect(item).to.have.attr('type');
+    });
 
-        //by className
-        cy.get('.btn.btn-primary')  // if there is a space in class name, delete the space and put dot (.). And start with a dot at the beginning
-        
-        // By id
-        cy.get('#wooden_spoon'); // we put # before id locator
+    // by attribute name
+    cy.get('[type]');
 
-        // if I want to use text: no xpath in Cypress as a locator, but it is still possible with a differen approach
-        cy.get('button').should('contain', 'Login').click();
+    // by className
+    cy.get('.btn.btn-primary'); // if there is a space in class name, delete the space and put dot (.). And start with a dot at the beginning
 
-    })
+    // By id
+    cy.get('#wooden_spoon'); // we put # before id locator
 
-    it('Check finding elements by travelling through DOM', () => {
-        // travel to find the login button: locate username box - go to parent form - then find button
-        cy.get('input[name="username"]').parents('form').find('button').should('contain', 'Login').click();
-    })
+    // if I want to use text: no xpath in Cypress as a locator, but it is still possible with a differen approach
+    cy.get('button').should('contain', 'Login').click();
+  });
 
-    it.only('Check Different Type of Assertions', () => {
-        // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
-        // Should Assertion: does the assertion directly on the object itself
-        cy.get('#wooden_spoon')
-        .should('contain','Login')
-        .and('have.class','btn btn-primary');
-        // expect assertion: creates a subject of our test, then you implement different actions 
-        cy.get('#wooden_spoon').then((buttonElement) => {
-            expect(buttonElement).to.have.text('Login');
-            expect(buttonElement).to.have.class('btn btn-primary');
-        })
-    })
-   
-}) 
+  it('Check finding elements by travelling through DOM', () => {
+    // travel to find the login button: locate username box - go to parent form - then find button
+    cy.get('input[name="username"]')
+      .parents('form')
+      .find('button')
+      .should('contain', 'Login')
+      .click();
+  });
+
+  it.only('Check Different Type of Assertions', () => {
+    // Cypress itself bundles assertions provided by Chai, Sinon and jQuery libraries
+    // Should Assertion: does the assertion directly on the object itself
+    cy.get('#wooden_spoon').should('contain', 'Login').and('have.class', 'btn btn-primary');
+    // expect assertion: creates a subject of our test, then you implement different actions
+    cy.get('#wooden_spoon').then((buttonElement) => {
+      expect(buttonElement).to.have.text('Login');
+      expect(buttonElement).to.have.class('btn btn-primary');
+    });
+  });
+});
--- cypress/e2e/testStructure.cy.js
+++ cypress/e2e/testStructure.cy.js
@@ -1,32 +1,32 @@
 /// <reference types="cypress" />
 
 describe('Context: My First Tests', () => {
-    before(() => {
-        // runs once before all test cases in this describe block, resembles beforeClass in TestNG
-    })
-    beforeEach(() => {
-        // runs before each test case, resembles BeforeMethod in TestNG
-        cy.clearCookies();
-    })
-    after(() => {
-        // similar to AfterClass in TestNG, runs once after all tests finish
-    })
-    afterEach(() => {
-        // similar to AfterMethod in TestNG
-    })
-    it.skip('Opening a web application', () => {
-        cy.visit('/registration_form');
-    })
-    xit('Test 2', () => {
-        expect(false).to.equal(false);
-    })
-    it('Test 3', () => {
-        expect(false).not.to.equal(true);
-    })
-    it('Test 4', () => {
-        expect(5).to.equal(5);
-    })
-    it('Test 5', () => {
-        expect(true).to.equal('5' ==5);
-    })
-}) 
+  before(() => {
+    // runs once before all test cases in this describe block, resembles beforeClass in TestNG
+  });
+  beforeEach(() => {
+    // runs before each test case, resembles BeforeMethod in TestNG
+    cy.clearCookies();
+  });
+  after(() => {
+    // similar to AfterClass in TestNG, runs once after all tests finish
+  });
+  afterEach(() => {
+    // similar to AfterMethod in TestNG
+  });
+  it.skip('Opening a web application', () => {
+    cy.visit('/registration_form');
+  });
+  xit('Test 2', () => {
+    expect(false).to.equal(false);
+  });
+  it('Test 3', () => {
+    expect(false).not.to.equal(true);
+  });
+  it('Test 4', () => {
+    expect(5).to.equal(5);
+  });
+  it('Test 5', () => {
+    expect(true).to.equal('5' == 5);
+  });
+});
